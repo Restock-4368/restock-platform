91ae3a77ecb7214491c50939244776c22e6bfae9
jahazielgg <u202319831@upc.edu.pe>
2025-06-07 17:26:32 -0500

    feat: add Recipe class to represent recipe entities with properties and ingredients

---
+++ Restock.Platform.API/Planning/Domain/Model/Entities/Recipe.cs
@@ -0,0 +1,19 @@
+﻿namespace Restock.Platform.API.Planning.Domain.Model.Entities;
+
+using Restock.Platform.API.Planning.Domain.Model.ValueObjects;
+
+public partial class Recipe
+{
+    public int Id { get; private set; }
+    public RecipeName Name { get; private set; }
+    public string? Description { get; private set; }
+
+    private readonly List<Ingredient> _ingredients = new();
+    public IReadOnlyCollection<Ingredient> Ingredients => _ingredients.AsReadOnly();
+
+    public Recipe(RecipeName name, string? description = null)
+    {
+        Name = name;
+        Description = description;
+    }
+}
