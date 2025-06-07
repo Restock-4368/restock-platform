cc1745c2f5696c206052d34bbc0d9692f204e10a
jahazielgg <u202319831@upc.edu.pe>
2025-06-07 17:26:16 -0500

    feat: add RecipeAggregate class to manage recipe entities

---
+++ Restock.Platform.API/Planning/Domain/Model/Aggregates/RecipeAgreggate.cs
@@ -0,0 +1,13 @@
+﻿namespace Restock.Platform.API.Planning.Domain.Model.Aggregates;
+
+using Restock.Platform.API.Planning.Domain.Model.Entities;
+
+public class RecipeAggregate
+{
+    public Recipe Root { get; }
+
+    public RecipeAggregate(Recipe recipe)
+    {
+        Root = recipe;
+    }
+}
