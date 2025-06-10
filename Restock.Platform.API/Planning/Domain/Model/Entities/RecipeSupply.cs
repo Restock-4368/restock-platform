4a559f55df337286ebe21ee673f9638606fc328a
jahazielgg <u202319831@upc.edu.pe>
2025-06-10 16:28:21 -0500

    feat: add RecipeSupply entity for managing supply identifiers and quantities

---
+++ Restock.Platform.API/Planning/Domain/Model/Entities/RecipeSupply.cs
@@ -0,0 +1,21 @@
+﻿using Restock.Platform.API.Planning.Domain.Model.ValueObjects;
+using Restock.Platform.API.Resource.Domain.Model.ValueObjects;
+
+namespace Restock.Platform.API.Planning.Domain.Model.Entities;
+
+public class RecipeSupply
+{
+    public SupplyIdentifier SupplyId { get; init; }
+    public RecipeQuantity Quantity { get; private set; }
+    
+    public RecipeSupply(SupplyIdentifier supplyId, RecipeQuantity quantity)
+    {
+        SupplyId = supplyId;
+        Quantity = quantity;
+    }
+    
+    public void UpdateQuantity(RecipeQuantity newQuantity)
+    {
+        Quantity = newQuantity;
+    }
+}
