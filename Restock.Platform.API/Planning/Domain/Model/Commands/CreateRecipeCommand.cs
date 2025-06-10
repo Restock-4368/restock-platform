1f61a76c5ad760665ee1884c0948932b7d17e80d
jahazielgg <u202319831@upc.edu.pe>
2025-06-10 16:26:34 -0500

    feat: add command classes for managing supplies and recipes in the domain model

---
+++ Restock.Platform.API/Planning/Domain/Model/Commands/CreateRecipeCommand.cs
@@ -0,0 +1,5 @@
+﻿namespace Restock.Platform.API.Planning.Domain.Model.Commands;
+
+using Restock.Platform.API.Planning.Domain.Model.ValueObjects;
+
+public record CreateRecipeCommand(string Name, string Description, string ImageUrl, decimal TotalPrice, int UserId);
