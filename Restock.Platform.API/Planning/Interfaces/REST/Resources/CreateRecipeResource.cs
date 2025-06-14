07907735a6c993b56a902c506f2ed0f3f82b1397
jahazielgg <u202319831@upc.edu.pe>
2025-06-13 21:15:33 -0500

    feat: add resource and assembler classes for creating and transforming recipe data

---
+++ Restock.Platform.API/Planning/Interfaces/REST/Resources/CreateRecipeResource.cs
@@ -0,0 +1,12 @@
+﻿namespace Restock.Platform.API.Planning.Interfaces.REST.Resources;
+
+public record SupplyInputResource(int SupplyId, int Quantity);
+
+public record CreateRecipeResource(
+    string Name,
+    string Description,
+    string ImageUrl,
+    decimal TotalPrice,
+    int UserId,
+    List<SupplyInputResource> Supplies
+);
