dcc3a370aab7bb3669e4333c86af627e9811644c
GabrielaShapiama28 <u202319448@upc.edu.pe>
2025-06-14 12:58:19 -0500

    feat(resource): add order to supplier states value object.

---
+++ Restock.Platform.API/Resource/Domain/Model/ValueObjects/EOrderToSupplierStates.cs
@@ -0,0 +1,9 @@
+namespace Restock.Platform.API.Resource.Domain.Model.ValueObjects;
+
+public enum EOrderToSupplierStates
+{
+    OnHold, //id = 1
+    Preparing, //id = 2
+    OnTheWay, //id = 3
+    Delivered //id = 4
+}
