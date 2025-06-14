3f13be28a15e2cce8f11be49fd35870f745e7172
jahazielgg <u202319831@upc.edu.pe>
2025-06-13 21:14:54 -0500

    feat: add IRecipeCommandService and IRecipeQueryService interfaces for recipe management

---
+++ Restock.Platform.API/Planning/Domain/Services/IRecipeCommandService.cs
@@ -0,0 +1,10 @@
+﻿using Restock.Platform.API.Planning.Domain.Model.Commands;
+
+namespace Restock.Platform.API.Planning.Domain.Services;
+
+public interface IRecipeCommandService
+{
+    Task<Guid> Handle(CreateRecipeCommand command);
+    Task Handle(UpdateRecipeCommand command);
+    Task Handle(DeleteRecipeCommand command);
+}
