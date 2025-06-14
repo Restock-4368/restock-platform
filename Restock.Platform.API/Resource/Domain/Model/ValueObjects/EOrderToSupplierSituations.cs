63f105597a0701449f9e4b219046d046ac2395cc
GabrielaShapiama28 <u202319448@upc.edu.pe>
2025-06-14 12:57:52 -0500

    feat(resource): add order to supplier situations value object.

---
+++ Restock.Platform.API/Resource/Domain/Model/ValueObjects/EOrderToSupplierSituations.cs
@@ -0,0 +1,9 @@
+namespace Restock.Platform.API.Resource.Domain.Model.ValueObjects;
+
+public enum EOrderToSupplierSituations
+{
+    Pending, //id = 1
+    Approved, //id = 2
+    Declined, //id = 3
+    Cancelled //id = 4
+}
