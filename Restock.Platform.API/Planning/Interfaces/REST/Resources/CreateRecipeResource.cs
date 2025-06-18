5fd17813b46bc63d7c3087190bca50a99bf7ed87
jahazielgg <u202319831@upc.edu.pe>
2025-06-18 12:34:48 -0500

    refactor(resource): remove Supplies input from CreateRecipeResource

--- Restock.Platform.API/Planning/Interfaces/REST/Resources/CreateRecipeResource.cs
+++ Restock.Platform.API/Planning/Interfaces/REST/Resources/CreateRecipeResource.cs
@@ -1,12 +1,10 @@
 ﻿namespace Restock.Platform.API.Planning.Interfaces.REST.Resources;
 
-public record SupplyInputResource(Guid SupplyId, double Quantity);
 
 public record CreateRecipeResource(
     string Name,
     string Description,
     string ImageUrl,
     decimal TotalPrice,
-    int UserId,
-    List<SupplyInputResource> Supplies
+    int UserId
 );
