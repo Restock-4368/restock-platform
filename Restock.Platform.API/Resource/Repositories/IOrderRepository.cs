b5bec2f01cd70b9955175f99f9fb090c8fc8efeb
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-06-22 09:55:30 -0500

    feat(orders): add order repository.

---
+++ Restock.Platform.API/Resource/Repositories/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using Restock.Platform.API.Resource.Domain.Model.Entities;
+
+namespace Restock.Platform.API.Resource.Repositories;
+
+public interface IOrderRepository
+{
+    Task<OrderToSupplier?> FindByIdAsync(Guid id, bool includeRequestedBatches = false);
+    Task<IEnumerable<OrderToSupplier>> ListAsync(bool includeSupplies = false);
+    Task<IEnumerable<OrderToSupplierBatch>> ListOrderToSupplierBatchesByOrderIdAsync(Guid orderId);
+    Task AddAsync(OrderToSupplier order);
+    void Update(OrderToSupplier order);
+    void Remove(OrderToSupplier order);
+}
