236becb0a6e0d61dc02e9e87afe64dcb47d02a48
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-06-24 00:52:15 -0500

    feat(resource): add order-to-supplier-batch created event.

---
+++ Restock.Platform.API/Resource/Domain/Model/Events/OrderToSupplierBatchCreatedEvent.cs
@@ -0,0 +1,8 @@
+﻿using Restock.Platform.API.Shared.Domain.Events;
+
+namespace Restock.Platform.API.Resource.Domain.Model.Events;
+
+public class OrderToSupplierBatchCreatedEvent(int batchId, double quantity, bool accepted) : IEvent
+{
+    
+}
