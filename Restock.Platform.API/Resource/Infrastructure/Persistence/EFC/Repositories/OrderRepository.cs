634c6d370014ffdece1e39e17ec225471fa8cd87
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-06-24 01:07:11 -0500

    feat(resource): add outbound services implementation.

---
+++ Restock.Platform.API/Resource/Infrastructure/Persistence/EFC/Repositories/OrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restock.Platform.API.Resource.Domain.Model.Aggregates;
+using Restock.Platform.API.Resource.Domain.Repositories;
+using Restock.Platform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using Restock.Platform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace Restock.Platform.API.Resource.Infrastructure.Persistence.EFC.Repositories;
+
+public class OrderRepository(AppDbContext context) : BaseRepository<OrderToSupplier>(context), IOrderRepository
+{
+    public async Task<IEnumerable<OrderToSupplier?>> LisyAsync()
+    {
+        return await Context.Set<OrderToSupplier>()
+            .ToListAsync();
+    }
+}
