0e998dcbe86cb31d53a8a65304d7e51e02aa7283
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-06-24 00:51:21 -0500

    refactor(resource): delete innecessary task in order repository.

--- Restock.Platform.API/Resource/Domain/Repositories/IOrderRepository.cs
+++ Restock.Platform.API/Resource/Domain/Repositories/IOrderRepository.cs
@@ -1,10 +1,9 @@
 ﻿ 
-using Restock.Platform.API.Resource.Domain.Model.Entities;
+using Restock.Platform.API.Resource.Domain.Model.Aggregates;
 using Restock.Platform.API.Shared.Domain.Repositories;
 
 namespace Restock.Platform.API.Resource.Domain.Repositories;
 
 public interface IOrderRepository : IBaseRepository<OrderToSupplier>
-{
-    Task<IEnumerable<OrderToSupplier>> ListAsync();
+{  
 }
