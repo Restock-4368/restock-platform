4f073f362ec95a55bcba8176da8ec52d2383d37c
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-06-24 09:31:07 -0500

    feat(resource): add batch repository interface.

---
+++ Restock.Platform.API/Resource/Domain/Repositories/IBatchRepository.cs
@@ -0,0 +1,10 @@
+﻿ 
+using Restock.Platform.API.Resource.Domain.Model.Entities;
+using Restock.Platform.API.Shared.Domain.Repositories;
+
+namespace Restock.Platform.API.Resource.Domain.Repositories;
+
+public interface IBatchRepository : IBaseRepository<Batch>
+{
+    Task<IEnumerable<Batch>> ListByIdsAsync(IEnumerable<int> batchIds);
+}
