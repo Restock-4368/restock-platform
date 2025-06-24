f02eef1901221aad0a37f295f41914fdd5a8d101
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-06-24 09:31:13 -0500

    feat(resource): add batch repository.

---
+++ Restock.Platform.API/Resource/Infrastructure/Persistence/EFC/Repositories/BatchRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore; 
+using Restock.Platform.API.Resource.Domain.Model.Entities;
+using Restock.Platform.API.Resource.Domain.Repositories;
+using Restock.Platform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using Restock.Platform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace Restock.Platform.API.Resource.Infrastructure.Persistence.EFC.Repositories;
+
+public class BatchRepository(AppDbContext context) : BaseRepository<Batch>(context), IBatchRepository
+{
+    public new async Task<IEnumerable<Batch>> ListAsync()
+    {
+        return await Context.Set<Batch>()
+            .ToListAsync();
+    }
+    
+    public async Task<IEnumerable<Batch>> ListByIdsAsync(IEnumerable<int> batchIds)
+    {
+        return await Context.Set<Batch>()
+            .Where(b => batchIds.Contains(b.BatchId))
+            .ToListAsync();
+    }
+}
