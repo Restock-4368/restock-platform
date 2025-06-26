b2976f37c7013963b13ccd0c8eeeeaa2e7ef3e3c
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-06-26 17:24:33 -0500

    feat(resource): add order to supplier batch assembler.

---
+++ Restock.Platform.API/Resource/Interfaces/REST/Transform/OrderToSupplierBatchResourceFromEntityAssembler.cs
@@ -0,0 +1,19 @@
+﻿ 
+using Restock.Platform.API.Resource.Domain.Model.Entities;
+using Restock.Platform.API.Resource.Interfaces.REST.Resources;
+
+namespace Restock.Platform.API.Resource.Interfaces.REST.Transform;
+
+public class OrderToSupplierBatchResourceFromEntityAssembler
+{
+    public static OrderToSupplierBatchResource ToResourceFromEntity(OrderToSupplierBatch orderToSupplierBatch)
+    {
+        return new OrderToSupplierBatchResource(
+            orderToSupplierBatch.OrderToSupplierId,  
+            orderToSupplierBatch.BatchId,  
+            orderToSupplierBatch.Quantity,
+            orderToSupplierBatch.Accepted
+        );
+    }
+}
+ 
