1778601be21448670cfd208961716d21b8709448
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-07-05 08:01:35 -0500

    feat(iam): add user persistence.

---
+++ Restock.Platform.API/IAM/Infrastructure/Persistence/EFC/Configuration/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restock.Platform.API.IAM.Domain.Model.Aggregates;
+
+namespace Restock.Platform.API.IAM.Infrastructure.Persistence.EFC.Configuration.Extensions;
+
+public static class ModelBuilderExtensions
+{
+    public static void ApplyIamConfiguration(this ModelBuilder builder)
+    {
+        // IAM Context
+        
+        builder.Entity<User>().HasKey(u => u.Id);
+        builder.Entity<User>().Property(u => u.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<User>().Property(u => u.Username).IsRequired();
+        builder.Entity<User>().Property(u => u.PasswordHash).IsRequired();
+    }
+}
