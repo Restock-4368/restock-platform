7230856dfb983c68146591919283c8cf9d291a9a
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-07-05 08:04:00 -0500

    feat(iam): add authentication assembler.

---
+++ Restock.Platform.API/IAM/Interfaces/REST/Transform/AuthenticatedUserResourceFromEntityAssembler.cs
@@ -0,0 +1,13 @@
+﻿using Restock.Platform.API.IAM.Domain.Model.Aggregates;
+using Restock.Platform.API.IAM.Interfaces.REST.Resources;
+
+namespace Restock.Platform.API.IAM.Interfaces.REST.Transform;
+
+public static class AuthenticatedUserResourceFromEntityAssembler
+{
+    public static AuthenticatedUserResource ToResourceFromEntity(
+        User user, string token)
+    {
+        return new AuthenticatedUserResource(user.Id, user.Username, token);
+    }
+}
