5561e8a9c364f4143e88140a57c93d66420c02d2
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-07-06 07:36:03 -0500

    feat(profiles): add business assemblers.

---
+++ Restock.Platform.API/Profiles/Interfaces/REST/Transform/CreateBusinessCommandFromResourceAssembler.cs
@@ -0,0 +1,18 @@
+﻿using Restock.Platform.API.Profiles.Domain.Model.Commands;
+using Restock.Platform.API.Profiles.Interfaces.REST.Resources;
+
+namespace Restock.Platform.API.Profiles.Interfaces.REST.Transform;
+ 
+public static class CreateBusinessCommandFromResourceAssembler
+{
+    public static CreateBusinessCommand ToCommandFromResource(CreateBusinessResource resource)
+    {
+        return new CreateBusinessCommand(
+            resource.Name,
+            resource.Email,
+            resource.Phone,
+            resource.Address,
+            resource.Categories
+        );
+    }
+}
