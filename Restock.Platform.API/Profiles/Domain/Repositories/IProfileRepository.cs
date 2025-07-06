6fd36c6d899ac81e7fe965b0a632c915f5c325cd
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-07-06 07:35:03 -0500

    feat(profiles): add profile repository.

---
+++ Restock.Platform.API/Profiles/Domain/Repositories/IProfileRepository.cs
@@ -0,0 +1,9 @@
+﻿using Restock.Platform.API.Profiles.Domain.Model.Aggregates;
+using Restock.Platform.API.Shared.Domain.Repositories;
+
+namespace Restock.Platform.API.Profiles.Domain.Repositories;
+
+public interface IProfileRepository : IBaseRepository<Profile>
+{
+    Task<Profile?> FindByEmailAsync(string email);
+}
---
+++ Restock.Platform.API/Profiles/Infrastructure/Persistence/EFC/Repositories/ProfileRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restock.Platform.API.Profiles.Domain.Model.Aggregates;
+using Restock.Platform.API.Profiles.Domain.Repositories;
+using Restock.Platform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using Restock.Platform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace Restock.Platform.API.Profiles.Infrastructure.Persistence.EFC.Repositories;
+
+public class ProfileRepository(AppDbContext context) : BaseRepository<Profile>(context), IProfileRepository
+{
+    public async Task<Profile?> FindByEmailAsync(string email)
+    {
+        return await Context.Set<Profile>().FirstOrDefaultAsync(p => p.Email == email);
+    }
+}
