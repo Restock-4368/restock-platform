acc1f38c4db3400dd10a67cb4ad88c283e481dcb
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-07-06 07:36:16 -0500

    feat(profiles): add profile assemblers.

---
+++ Restock.Platform.API/Profiles/Interfaces/REST/Transform/CreateProfileCommandFromResourceAssembler.cs
@@ -0,0 +1,21 @@
+﻿using Restock.Platform.API.Profiles.Domain.Model.Commands;
+using Restock.Platform.API.Profiles.Interfaces.REST.Resources;
+
+namespace Restock.Platform.API.Profiles.Interfaces.REST.Transform;
+
+public static class CreateProfileCommandFromResourceAssembler
+{
+    public static CreateProfileCommand ToCommandFromResource(CreateProfileResource resource)
+    {
+        return new CreateProfileCommand(
+            resource.FirstName,
+            resource.LastName,
+            resource.Email,
+            resource.Phone,
+            resource.Address,
+            resource.Country,
+            resource.UserId,
+            resource.BusinessId
+        );
+    }
+}
---
+++ Restock.Platform.API/Profiles/Interfaces/REST/Transform/ProfileResourceFromEntityAssembler.cs
@@ -0,0 +1,20 @@
+﻿using Restock.Platform.API.Profiles.Domain.Model.Aggregates;
+using Restock.Platform.API.Profiles.Interfaces.REST.Resources;
+
+namespace Restock.Platform.API.Profiles.Interfaces.REST.Transform;
+ 
+public static class ProfileResourceFromEntityAssembler
+{
+    public static ProfileResource ToResourceFromEntity(Profile entity)
+    {
+        return new ProfileResource(
+            entity.Id, 
+            entity.FullName, 
+            entity.Email, 
+            entity.Phone, 
+            entity.Address, 
+            entity.Country,
+            entity.UserId.Value,
+            entity.BusinessId);
+    }
+}
