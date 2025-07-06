2274c831f54d8c209108a72f87350e1108568a6c
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-07-06 07:31:27 -0500

    feat(resource): add profile aggregate.

---
+++ Restock.Platform.API/Profiles/Domain/Model/Aggregates/Profile.cs
@@ -0,0 +1,67 @@
+﻿using Restock.Platform.API.Profiles.Domain.Model.Commands;
+using Restock.Platform.API.Profiles.Domain.Model.ValueObjects;
+
+namespace Restock.Platform.API.Profiles.Domain.Model.Aggregates;
+
+public class Profile
+{
+    public int Id { get; }
+    public PersonName Name { get; private set; }
+    public string Email { get; private set; }
+    public string Address { get; private set; }
+    
+    public string Phone { get; private set; } 
+    
+    public string Country { get; private set; } 
+    
+    public UserId UserId { get; private set; }
+    
+    public int BusinessId { get; private set; }
+    
+    public string FullName => Name.FullName; 
+
+    public Profile()
+    {
+        Name = new PersonName();
+        Email = string.Empty;
+        Address = string.Empty;
+        Phone = string.Empty;
+        Country = string.Empty;
+    }
+    
+    public Profile(string firstName, string lastName, string email, string phone, string address, string country, int userId, int businessId)
+    {
+        Name = new PersonName(firstName, lastName);
+        Email = email;
+        Address = address;
+        Phone = phone;
+        Country = country;
+        UserId = new UserId(userId);
+        BusinessId = businessId;
+    }
+
+    public Profile(CreateProfileCommand command) : this(
+        command.FirstName, 
+        command.LastName, 
+        command.Email,
+        command.Phone, 
+        command.Address, 
+        command.Country,
+        command.UserId,
+        command.BusinessId)
+    {
+        
+    }
+    
+    public void Update(string? firstName, string? lastName, string? email, 
+        string? phone, string? address, string? country)
+    {   
+        if(firstName != null && lastName != null)
+            Name = new PersonName(firstName, lastName);
+        
+        Email = email ?? Email;
+        Address = address ?? Address; 
+        Phone = phone ?? Phone; 
+        Country = country ?? Country; 
+    }
+}
