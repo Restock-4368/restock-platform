223a7779869fec75f01542b643db2b60bc8a6b40
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-07-06 07:35:50 -0500

    feat(profiles): add business resources.

---
+++ Restock.Platform.API/Profiles/Interfaces/REST/Resources/UpdateBusinessResource.cs
@@ -0,0 +1,9 @@
+﻿namespace Restock.Platform.API.Profiles.Interfaces.REST.Resources;
+
+public record UpdateBusinessResource(
+    string Id, 
+    string Name,
+    string Email, 
+    string Phone,
+    string Address, 
+    string Categories);
