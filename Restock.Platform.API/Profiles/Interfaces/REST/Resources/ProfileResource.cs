cf2bb3041660fa3c07216c59d224572c89efd132
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-07-06 07:35:35 -0500

    feat(profiles): add profile resources.

---
+++ Restock.Platform.API/Profiles/Interfaces/REST/Resources/ProfileResource.cs
@@ -0,0 +1,11 @@
+﻿namespace Restock.Platform.API.Profiles.Interfaces.REST.Resources;
+
+public record ProfileResource(
+    int Id,
+    string FullName,
+    string Email,
+    string Phone,
+    string Address,
+    string Country, 
+    int UserId,
+    int BusinessId);
---
+++ Restock.Platform.API/Profiles/Interfaces/REST/Resources/UpdateProfileResource.cs
@@ -0,0 +1,13 @@
+﻿namespace Restock.Platform.API.Profiles.Interfaces.REST.Resources;
+
+public record UpdateProfileResource(
+    string Id, 
+    string FirstName,
+    string LastName,
+    string Email, 
+    string Phone,
+    string Address, 
+    string Country)
+{
+    public string EstimatedShipTime { get; set; }
+}
