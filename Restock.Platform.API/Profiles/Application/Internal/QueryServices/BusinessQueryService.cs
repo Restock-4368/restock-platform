047d6a1d8c890f7e686d0d72d0c362eb5fc468c9
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-07-06 07:34:36 -0500

    feat(profiles): add business query service.

---
+++ Restock.Platform.API/Profiles/Application/Internal/QueryServices/BusinessQueryService.cs
@@ -0,0 +1,21 @@
+﻿using Restock.Platform.API.Profiles.Domain.Model.Aggregates;
+using Restock.Platform.API.Profiles.Domain.Model.Entities;
+using Restock.Platform.API.Profiles.Domain.Model.Queries;
+using Restock.Platform.API.Profiles.Domain.Repositories;
+using Restock.Platform.API.Profiles.Domain.Services;
+
+namespace Restock.Platform.API.Profiles.Application.Internal.QueryServices;
+
+public class BusinessQueryService(
+    IBusinessRepository businessRepository): IBusinessQueryService
+{
+    public async Task<IEnumerable<Business?>> Handle(GetAllBusinessesQuery query)
+    {
+        return await businessRepository.ListAsync();
+    }
+
+    public async Task<Business?> Handle(GetBusinessByIdQuery query)
+    {
+        return await businessRepository.FindByIdAsync(query.BusinessId);
+    } 
+}
---
+++ Restock.Platform.API/Profiles/Domain/Services/IBusinessQueryService.cs
@@ -0,0 +1,11 @@
+﻿using Restock.Platform.API.Profiles.Domain.Model.Aggregates;
+using Restock.Platform.API.Profiles.Domain.Model.Entities;
+using Restock.Platform.API.Profiles.Domain.Model.Queries;
+
+namespace Restock.Platform.API.Profiles.Domain.Services;
+ 
+public interface IBusinessQueryService
+{
+    Task<IEnumerable<Business?>> Handle(GetAllBusinessesQuery query);
+    Task<Business?> Handle(GetBusinessByIdQuery query); 
+}
