a882dbfa5cda01d486cbe30da70ff5a2dd033cb4
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-07-06 07:34:51 -0500

    feat(profiles): add profile query service.

---
+++ Restock.Platform.API/Profiles/Application/Internal/QueryServices/ProfileQueryService.cs
@@ -0,0 +1,25 @@
+﻿using Restock.Platform.API.Profiles.Domain.Model.Aggregates;
+using Restock.Platform.API.Profiles.Domain.Model.Queries;
+using Restock.Platform.API.Profiles.Domain.Repositories;
+using Restock.Platform.API.Profiles.Domain.Services;
+
+namespace Restock.Platform.API.Profiles.Application.Internal.QueryServices;
+
+public class ProfileQueryService(
+    IProfileRepository profileRepository): IProfileQueryService
+{
+    public async Task<IEnumerable<Profile>> Handle(GetAllProfilesQuery query)
+    {
+        return await profileRepository.ListAsync();
+    }
+
+    public async Task<Profile?> Handle(GetProfileByIdQuery query)
+    {
+        return await profileRepository.FindByIdAsync(query.ProfileId);
+    } 
+
+    public async Task<Profile?> Handle(GetProfileByEmailQuery query)
+    {
+        return await profileRepository.FindByEmailAsync(query.Email);
+    }
+}
---
+++ Restock.Platform.API/Profiles/Domain/Services/IProfileQueryService.cs
@@ -0,0 +1,12 @@
+﻿using Restock.Platform.API.Profiles.Domain.Model.Aggregates;
+using Restock.Platform.API.Profiles.Domain.Model.Queries;
+
+namespace Restock.Platform.API.Profiles.Domain.Services;
+ 
+public interface IProfileQueryService
+{
+    Task<IEnumerable<Profile>> Handle(GetAllProfilesQuery query);
+    Task<Profile?> Handle(GetProfileByIdQuery query); 
+    
+    Task<Profile?> Handle(GetProfileByEmailQuery query);
+}
