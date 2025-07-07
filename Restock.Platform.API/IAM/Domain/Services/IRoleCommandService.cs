e5134595cdce75ab010df8b6ec837b867959be53
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-07-06 20:19:22 -0500

    feat(iam): add role command service.

---
+++ Restock.Platform.API/IAM/Domain/Services/IRoleCommandService.cs
@@ -0,0 +1,8 @@
+﻿using Restock.Platform.API.IAM.Domain.Model.Commands;
+
+namespace Restock.Platform.API.IAM.Domain.Services;
+
+public interface IRoleCommandService
+{
+    Task Handle(SeedRolesCommand command);
+}
